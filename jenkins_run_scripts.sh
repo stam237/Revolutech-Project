bc65243a76fac4472f780338493509e550f8100b
Ahmadou <ahmadousop9@gmail.com>
2023-04-10 02:00:26 -0400

    modif1 jenkins

---
+++ jenkins_run_scripts.sh
@@ -0,0 +1,13 @@
+#!\bin\sh
+docker run \
+  --name jenkins-docker \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 5000:2376 \
+  docker:dind \
+  --storage-driver overlay2
